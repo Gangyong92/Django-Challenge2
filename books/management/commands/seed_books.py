86485d9bf25d84d7b0a4a00193dfc7fbb6a9d07e
Gangyong Hyeong <hyeonggang@naver.com>
2020-12-07 01:13:25 +0900

    #10 Django-Challenge

--- books/management/commands/seed_books.py
+++ books/management/commands/seed_books.py
@@ -10,13 +10,14 @@ class Command(BaseCommand):
 
     help = "This command seeds books"
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--total", help="How many books do you want to create?", default=10
-        )
+    # def add_arguments(self, parser):
+    #     parser.add_argument(
+    #         "--total", help="How many books do you want to create?", default=10
+    #     )
 
     def handle(self, *args, **options):
-        total = int(options.get("total"))
+        # total = int(options.get("total"))
+        total = 40
         categories = Category.objects.all()
         writers = Person.objects.filter(kind=Person.KIND_WRITER)
         seeder = Seed.seeder()
