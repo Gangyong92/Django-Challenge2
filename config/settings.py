8a23c2c177c3ecf5aaa071d28ea49a56179b4d6b
Gangyong Hyeong <hyeonggang@naver.com>
2020-12-06 20:58:27 +0900

    #10 Django-Challenge

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET", "JmvnBptT6f%uwGsG27kUHA1")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com", "localhost"]
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 
 # Application definition
